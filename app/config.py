2c6e84e2eaec1b2f04bf77a79d696f88f60648e8
John <johnjohnjohnkimchi@gmail.com>
2026-05-22 15:10:59 -0400

    Removed duplicate lines

--- app/config.py
+++ app/config.py
@@ -2,15 +2,15 @@ from pydantic_settings import BaseSettings
 
 class Env(BaseSettings):
     DATABASE_HOSTNAME: str
+    DATABASE_PORT: int
     DATABASE_USERNAME: str
     DATABASE_PASSWORD: str
     DATABASE_NAME: str
-    DATABASE_PORT: str
     SECRET_KEY: str
     # ALGORITHM: str
     # ACCESS_TOKEN_EXPIRE_MINUTES: str # might not need this
 
-    class config:
+    class Config:
         env_file = ".env"
 
 env = Env()
