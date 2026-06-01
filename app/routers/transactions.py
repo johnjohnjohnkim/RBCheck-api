15ef62948bcf32ed51135bc9d7fd58a315cc662e
john <johnjohnjohnkimchi@gmail.com>
2026-06-01 16:02:05 -0400

    all methods implemented, refactored duplicate code

--- app/routers/transactions.py
+++ app/routers/transactions.py
@@ -3,7 +3,7 @@ from fastapi import status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 from ..database import get_db
 from typing import List
-from datetime import datetime, time
+from datetime import datetime, time, timedelta, date
 from decimal import Decimal
 
 router = APIRouter(
@@ -39,11 +39,11 @@ def send_transaction(transactions: schemas.Transaction, db: Session = Depends(ge
     
 
 @router.patch("/{id}", response_model=schemas.Transaction, status_code=status.HTTP_200_OK)
-def update_transaction(id: int, updated_input: schemas. UpdateTransaction, db: Session = Depends(get_db)):
+def update_transaction(id: int, updated_input: schemas.UpdateTransaction, db: Session = Depends(get_db)):
     transaction = db.query(models.Transaction).filter(models.Transaction.transaction_id == id).first()
     if not transaction:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Transaction could not be found.")
-
+     
     updated_transaction = updated_input.model_dump(exclude_unset=True)
 
     for key, value in updated_transaction.items():
@@ -55,29 +55,60 @@ def update_transaction(id: int, updated_input: schemas. UpdateTransaction, db: S
 
 @router.get("/weekly", response_model=List[schemas.Transaction])
 def current_week_transactions():
+    # List of all transactions starting from Sunday or Monday, depending on User's preference?
+    # can definitely just make it toggle it's no biggie
     pass
 
 @router.get("/past_7_days", response_model=List[schemas.Transaction])
-def past_7_days_transactions():
-    pass
+def past_7_days_transactions(db: Session = Depends(get_db)):
+    curr = date.today()
+    delta = timedelta(days=7)
+
+    start = curr - delta
+
+    query_start = datetime.combine(start, time.min)
+    query_end = datetime.combine(curr, time.max)
+
+    results = db.query(models.Transaction).filter(models.Transaction.transaction_datetime>= query_start, models.Transaction.transaction_datetime<=query_end).all()
+
+    return results
 
 @router.get("/monthly", response_model=List[schemas.Transaction])
-def current_month_transactions():
-    pass
+def current_month_transactions(db: Session = Depends(get_db)):
+    curr = date.today()
+    delta = timedelta(days=curr.day-1)
+
+    start_of_month = curr - delta
+
+    query_month_start = datetime.combine(start_of_month, time.min)
+    query_month_end = datetime.combine(curr, time.max)
+
+    results = db.query(models.Transaction).filter(models.Transaction.transaction_datetime>= query_month_start, models.Transaction.transaction_datetime<=query_month_end).all()
+    return results
 
 @router.get("/biweekly", response_model=List[schemas.Transaction])
 def biweekly_transactions():
+    # Unsure of the best way to do this. Probably rolling basis? just not sure if this is necessary to implement.
     pass
 
 @router.get("/date_range", response_model=List[schemas.Transaction])
 def transactions_by_date_range(start_date: str, end_date: str, db: Session = Depends(get_db)):
+    # If end date not provided, set to current date?
     pass
 
+@router.get("/merchant", response_model = List[schemas.Transaction])
 def transactions_at_merchant(merchant: str, db: Session = Depends(get_db)):
-    pass
+    results = db.query(models.Transaction).filter(models.Transaction.place.ilike(f'%{merchant}%')).all()
 
-def transaction_amount_range(amount_start: Decimal, amount_end: Decimal, Session = Depends(get_db)):
-    pass
+    return results
+    
+@router.get("/price_range", response_model=List[schemas.Transaction]) 
+def transaction_amount_range(range_start: Decimal, range_end: Decimal, db: Session = Depends(get_db)):
+    max_amount = max(range_start, range_end)
+    min_amount = min(range_start, range_end)
+    results = db.query(models.Transaction).filter(models.Transaction.amount >= min_amount, models.Transaction.amount <= max_amount).all()
+    
+    return results
 
 @router.get("/{id}", status_code = status.HTTP_200_OK, response_model=schemas.Transaction)
 def get_one_transaction(id: int, db: Session = Depends(get_db)):
